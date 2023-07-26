132ba5ee76089b0773c93e7a38bb142fe4fc8ed0
carlangas2503 <carlos.valencias2503@gmail.com>
2023-07-26 14:43:27 -0500

    muestra de errores

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,16 +1,27 @@
 import { useState} from 'react'
 import SearchBar from "../../components/SearchBar/SearchBar";
 import Results  from '../../components/Results/Results';
+import ErrorSearch from '../../components/ErrorSearch/ErrorSearch';
 import axios from 'axios'
 import Navbar from '../../components/Navbar/Navbar';
 export default function Home (){
-    const[ ver ,letVer ] = useState([])
+    const[ ver ,setVer ] = useState([])
+    const[ error, setError ] = useState('')
     
 
-    const onSearch = async(bar)=>{
-        const respApi = await axios(`http://localhost:3001/products/${Number(bar)}`)
-        console.log(respApi.data);
-        return letVer([...ver,respApi.data])
+    const onSearch = async(bar,setNoVer)=>{
+        try {
+            const respApi = await axios(`http://localhost:3001/products/${Number(bar)}`)
+            setNoVer(true)
+            setError('')
+            return setVer([...ver,respApi.data])  
+        } catch (error) {
+            setNoVer(false)
+            setVer([])
+            const resError = error.request.response.split('"mensaje":')[1].split('}')[0].split('"')[1]
+            return setError(resError)
+        }
+        
     }
     return(
         <div>
@@ -18,7 +29,10 @@ export default function Home (){
                <SearchBar onSearch={onSearch}/> 
             </div>
             <div>
-                <Results ver={ver}/>
+                {ver?<Results ver={ver}/>:null}
+            </div>
+            <div>
+                {error?<ErrorSearch error={error}/>:null}
             </div>
         </div>
     )
